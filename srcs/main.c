a99b0d8d5da6ffd3cae968ec329ab9f40540fb9e
Aurelien FONTAINE <afontain@e3r6p3.clusters.42paris.fr>
2023-03-23 15:31:08 +0100

    Premier Push

---
+++ srcs/main.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afontain <afontain@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 13:08:56 by afontain          #+#    #+#             */
+/*   Updated: 2023/03/23 14:43:08 by afontain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void	img_pix_put(t_img *img, int x, int y, int color)
+{
+	char    *pixel;
+	int		i;
+
+	i = img->bpp - 8;
+    pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	while (i >= 0)
+	{
+		/* big endian, MSB is the leftmost bit */
+		if (img->endian != 0)
+			*pixel++ = (color >> i) & 0xFF;
+		/* little endian, LSB is the leftmost bit */
+		else
+			*pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
+		i -= 8;
+	}
+}
+
+int	main(void)
+{
+	t_data	data;
+
+	data.mlx_ptr = mlx_init();
+	if (!data.mlx_ptr)
+		return (MLX_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
+								"Fractol");
+	if (!data.win_ptr)
+	{
+		free(data.win_ptr);
+		return (MLX_ERROR);
+	}
+	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	
+	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp,
+			&data.img.line_len, &data.img.endian);
+	// /* Setup hooks */ 
+	mlx_loop_hook(data.mlx_ptr, &render, &data);
+	
+	// Suppr la fenetre avec ESC et le X de la fenetre
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+	mlx_hook(data.win_ptr, ClientMessage, KeyPressMask, &handle_mousepress, &data);
+
+	mlx_loop(data.mlx_ptr);
+	
+	/*if there's no window left, execute this code */
+	mlx_destroy_image(data.mlx_ptr, data.img.mlx_img);
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
+}
