5fabaf7b8ac929f98eefd19a00d1b30f9dd9bbb0
Aurelien FONTAINE <afontain@e2r7p15.clusters.42paris.fr>
2023-04-25 20:41:03 +0200

    NORMALEMENT CA MARCHE HEIN

--- srcs/move.c
+++ srcs/move.c
@@ -6,7 +6,7 @@
 /*   By: afontain <afontain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 11:29:27 by afontain          #+#    #+#             */
-/*   Updated: 2023/04/25 17:18:30 by afontain         ###   ########.fr       */
+/*   Updated: 2023/04/25 17:56:46 by afontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void	move(t_data *data, double move, char direction)
 		data->xmin = data->xmin + (move / (pow(2.0, (double)data->count_zoom)));
 	}
 }
-//  cz = 0 d = 100 m = 0.2
-//  cz = 1 d = 50  m = 0.1
-//  cz = 2 d = 25  m = 0.05
-//  cz = 3 d = 12.5 m = 0.025
+//  c_z = 0 d = 3 m = 0.2
+//  c_z = 1 d = 1.5  m = 0.1
+//  c_z = 2 d = 0.75 m = 0.05
+//  c_z = 3 d = 0.375 m = 0.025
